5aaaffbbdf0fc9fb209df7e059f5ba03fa6246a8
Oleg Anisimov <oleg20027855@mail.ru>
2024-02-03 23:53:32 +0700

    MPI first and second task

---
+++ MPI/MPI.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <mpi.h>
+#include <boost/uuid/detail/md5.hpp>
+#include <boost/algorithm/hex.hpp>
+#include <ctime>
+
+std::string generateRandomPassword(int length) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    std::string password;
+    for (int i = 0; i < length; ++i) {
+        password += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    return password;
+}
+
+std::string calculateMD5(const std::string& input) {
+    boost::uuids::detail::md5 md5;
+    md5.process_bytes(input.data(), input.size());
+
+    boost::uuids::detail::md5::digest_type digest;
+    md5.get_digest(digest);
+
+    std::string md5Hash;
+    boost::algorithm::hex(reinterpret_cast<const unsigned char*>(&digest), reinterpret_cast<const unsigned char*>(&digest) + sizeof(digest), std::back_inserter(md5Hash));
+    return md5Hash;
+}
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    const int passwordLength = 5;
+
+    srand(time(NULL) + rank);
+
+    std::string targetHash;
+    bool passwordFound = false;
+
+    // Генерация пароля и хэша в процессе с рангом 0
+    if (rank == 0) {
+        std::string generatedPassword = generateRandomPassword(passwordLength);
+        std::cout << "Generated Password: " << generatedPassword << std::endl;
+
+        targetHash = calculateMD5(generatedPassword);
+        std::cout << "Generated Hash: " << targetHash << std::endl;
+
+        // Отправка целевого хэша другим процессам
+        for (int dest = 1; dest < size; ++dest) {
+            MPI_Send(targetHash.c_str(), targetHash.size() + 1, MPI_CHAR, dest, 0, MPI_COMM_WORLD);
+        }
+    }
+    else {
+        // Принятие целевого хэша от процесса с рангом 0
+        char receivedHash[33];
+        MPI_Recv(receivedHash, 33, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        targetHash = receivedHash;
+    }
+
+    // Генерация паролей и сравнение хэшей в цикле
+    while (!passwordFound) {
+        std::string generatedPassword = generateRandomPassword(passwordLength);
+        std::string generatedHash = calculateMD5(generatedPassword);
+
+        if (generatedHash == targetHash) {
+            std::cout << "Rank " << rank << ": Password found! Password: " << generatedPassword << ", Hash: " << generatedHash << std::endl;
+            passwordFound = true; // Устанавливаем флаг, чтобы прекратить генерацию
+        }
+
+        // Каждый процесс проверяет флаг после каждой итерации цикла
+        MPI_Allreduce(MPI_IN_PLACE, &passwordFound, 1, MPI_C_BOOL, MPI_LOR, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ MPI/Task2.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <mpi.h>
+#include <chrono>
+
+using namespace std;
+
+MPI_Status status;
+
+int main(int argc, char* argv[]) {
+    int size = 4096;
+
+    __int64* Am = new __int64[size * size];
+    __int64* Bm = new __int64[size * size];
+    __int64* Rs = new __int64[size * size];
+    __int64* Rt = new __int64[size * size];
+
+    for (int i = 0; i < size * size; i++) {
+        Am[i] = rand() % 10;
+        Bm[i] = rand() % 10;
+        Rs[i] = 0;
+        Rt[i] = 0;
+    }
+
+    auto startScalar = chrono::system_clock::now();
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            for (int k = 0; k < size; ++k) {
+                Rs[i * size + j] += Am[i * size + k] * Bm[k * size + j];
+            }
+        }
+    }
+
+    auto endScalar = chrono::system_clock::now();
+    chrono::duration<double> diffScalar = endScalar - startScalar;
+
+    MPI_Init(&argc, &argv);
+
+    int rank, sizeProc, source, blockSize, offset;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &sizeProc);
+
+    if (rank == 0) {
+        auto start = chrono::system_clock::now();
+        blockSize = size / sizeProc;
+        offset = 0;
+
+        for (int dest = 1; dest < sizeProc; dest++) {
+            int currentBlockSize = blockSize + (dest <= (size % sizeProc) ? 1 : 0);
+            MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+            MPI_Send(&currentBlockSize, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+            MPI_Send(&Am[offset * size], currentBlockSize * size, MPI_LONG_LONG, dest, 1, MPI_COMM_WORLD);
+            MPI_Send(Bm, size * size, MPI_LONG_LONG, dest, 1, MPI_COMM_WORLD);
+            offset += currentBlockSize;
+        }
+
+        for (int i = offset; i < offset + blockSize; i++) {
+            for (int j = 0; j < size; j++) {
+                Rt[i * size + j] = 0;
+                for (int k = 0; k < size; k++) {
+                    Rt[i * size + j] += Am[i * size + k] * Bm[k * size + j];
+                }
+            }
+        }
+
+        for (int i = 1; i < sizeProc; i++) {
+            int currentBlockSize = blockSize + (i <= (size % sizeProc) ? 1 : 0);
+            MPI_Recv(&offset, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+            MPI_Recv(&currentBlockSize, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+            MPI_Recv(&Rt[offset * size], currentBlockSize * size, MPI_LONG_LONG, i, 2, MPI_COMM_WORLD, &status);
+        }
+
+        auto end = chrono::system_clock::now();
+        chrono::duration<double> diff = end - start;
+        cout << "Time MPI: " << diff.count() << " s" << endl;
+        cout << "Time scalar: " << diffScalar.count() << " s" << endl;
+
+        bool equal = true;
+        for (int i = 0; i < size * size; i++) {
+            if (Rs[i] != Rt[i]) {
+                equal = false;
+                break;
+            }
+        }
+
+        if (equal) {
+            cout << "Matrix is equal" << endl;
+        }
+        else {
+            cout << "Matrix is not equal" << endl;
+        }
+
+        delete[] Am;
+        delete[] Bm;
+        delete[] Rs;
+        delete[] Rt;
+
+    }
+    else {
+        MPI_Recv(&offset, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&blockSize, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        Am = new __int64[blockSize * size];
+        MPI_Recv(Am, blockSize * size, MPI_LONG_LONG, 0, 1, MPI_COMM_WORLD, &status);
+        Bm = new __int64[size * size];
+        MPI_Recv(Bm, size * size, MPI_LONG_LONG, 0, 1, MPI_COMM_WORLD, &status);
+
+        Rt = new __int64[blockSize * size];
+
+        for (int i = 0; i < blockSize; i++) {
+            for (int j = 0; j < size; j++) {
+                Rt[i * size + j] = 0;
+                for (int k = 0; k < size; k++) {
+                    Rt[i * size + j] += Am[i * size + k] * Bm[k * size + j];
+                }
+            }
+        }
+
+        MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        MPI_Send(&blockSize, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+        MPI_Send(Rt, blockSize * size, MPI_LONG_LONG, 0, 2, MPI_COMM_WORLD);
+
+        delete[] Am;
+        delete[] Bm;
+        delete[] Rt;
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
